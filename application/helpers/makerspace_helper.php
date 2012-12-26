058eb4acf8ba4f4e35470d382a4ee1294ae09f36
Stockholm Makerspace <info@makerspace.se>
2012-12-27 00:24:36 +0100

    Added gravatar helper

--- application/helpers/makerspace_helper.php
+++ application/helpers/makerspace_helper.php
@@ -1,11 +1,18 @@
 <?php
 
 /**
- * Helper for Internal
+ * Gravatar Helper
+ * @author Jim Nelin
  */
-
-
-function is_loggedin() {
-	// ToDO: Session-based.
-	return false;
+function gravatar($email, $rating = 'pg', $size = 32, $default = 'retro') {
+	
+	// optional options
+	$options = array();
+	if ($rating) $options[] = "rating=$rating";
+	if ($size) $options[] = "size=$size";
+	if ($default) $options[] = "default=$default";
+	
+	// put together the URL and return it
+	return 'https://secure.gravatar.com/avatar/' . md5(strtolower(trim($email))) . implode($options, '&');
 }
+
