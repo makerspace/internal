9c278fea61bdad36296642524d33442fc34b3259
Stockholm Makerspace <info@makerspace.se>
2012-12-27 22:12:43 +0100

    SQL

--- internal.sql
+++ internal.sql
@@ -9,7 +9,7 @@ CREATE TABLE `acl` (
   PRIMARY KEY (`user_id`)
 );
 CREATE TABLE `config` (
-  `key` varchar(64) NOT NULL COMMENT 'Config-key',
+  `key` varchar(64) NOT NULL COMMENT 'config key-name ($this->dbconfig->KEY)',
   `value` varchar(2048) NOT NULL,
   `desc` varchar(255) DEFAULT NULL COMMENT 'Optional description of config',
   PRIMARY KEY (`key`)
