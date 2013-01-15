1964ea25a9d9f24c46687216d3a98c1d3d4b2b7f
Jim Nelin <jim.nelin@makerspace.se>
2013-01-15 16:25:33 +0100

    Smaller updated.

--- application/views/todo/listing.php
+++ application/views/todo/listing.php
@@ -1,6 +1,10 @@
-<h2>ToDo - <?php echo ucfirst(ENVIRONMENT); ?></h2>
+<h2>ToDo <small>(<?php echo ucfirst(ENVIRONMENT); ?> Environment)</small></h2>
 
 <pre>
-* ????
-* ???
+There might be some fancy custom issue/ToDo tracker here some day.
+Perhaps based upon JIRA or similar.
+
+But for now - we're using GitHub Issues instead.
 </pre>
+
+<a href="https://github.com/makerspace/internal.makerspace.se/issues" class="btn btn-info btn-large">Show issues on GitHub</a>
