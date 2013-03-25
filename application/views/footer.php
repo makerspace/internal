a11bffb0bca9d366edf03b919d17da21d71909a1
Jim Nelin <jim.nelin@makerspace.se>
2013-03-25 04:15:06 +0100

    Added a linebreak

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,6 @@
 	</div>
+	<br>
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 	<script src="/assets/js/bootstrap.min.js"></script>
 	<script src="/assets/js/makerspace.js"></script>
