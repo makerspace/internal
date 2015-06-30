28dd2afa5f835a548a476151a3d4ba1fa8e65b98
Christian Antila <tagged-github.com@chille.se>
2015-06-30 16:43:22 +0200

    Updated Laravel configuration

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://internal.dev',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'SvglAWl2dL0ugPmDrqIvi6bE4oghdV1b'),
 
     'cipher' => 'AES-256-CBC',
 
