a7bcdcec08720af0f4abf7508c8f87f0d68d9a0b
Christian Antila <tagged-github.com@chille.se>
2015-07-03 21:46:50 +0200

    Added NTP to virtual machine

--- setup/other.sh
+++ setup/other.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y tcsh
+sudo apt-get install -y ntp
 
 # curl -sS https://getcomposer.org/installer | php
 # php composer.phar global require "laravel/installer=~1.1"
