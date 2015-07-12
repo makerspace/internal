9c71444f2cf31543ce398e24ffedfc66fe037f27
Johan Husman <johusman@gmail.com>
2015-07-12 18:19:02 +0200

    Test data generation:   * Added generation for member_rfid, and made some modifications to members code, including civic reg number uniqueness check against database.   * General improvements, and report on special cases at end of run.

--- database/generate/generate_testdata.py
+++ database/generate/generate_testdata.py
@@ -2,12 +2,20 @@
 
 import MySQLdb
 import members
+import member_rfid
 
 db = MySQLdb.connect(user='internal', passwd='g34Api5C9L', host='127.0.0.1', db='internal', charset='utf8')
 
 try:
-    new_member_ids = members.generate(db, 100)
+    report = []
+    new_members = members.generate(db, report, 1000)
+    member_rfid.generate(db, report, new_members)
     db.commit()
+
+    print "Done!\n"
+    print "Special test cases generated:"
+    for report_line in report:
+        print "%s: %s" % (report_line[1].civicregno, report_line[0])
 except:
     db.rollback()
     raise 
