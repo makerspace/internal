744c338cf9c2c007f2dd67a6a731db4fb4238333
Christian Antila <tagged-github.com@chille.se>
2015-11-19 14:43:06 +0100

    Removed JS files and updated index HTML to include a transpiled JS file.

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -4,31 +4,26 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Internal v2.0</title>
+
 	<link rel="shortcut icon" href="/gfx/favicon.ico" type="image/x-icon">
 	<link rel="apple-touch-icon-precomposed" href="/gfx/apple-touch-icon.png">
 
 	<script src="/js/libs/jquery.min.js"></script>
-	<script src="/js/libs/JSXTransformer.js"></script>
-	<script src="/js/libs/react.js"></script>
-	<script src="/js/libs/ReactRouter.js"></script>
-	<script src="/js/libs/underscore.js"></script>
-	<script src="/js/libs/backbone.js"></script>
-	<script src="/js/libs/backbone-react-component.js"></script>
-	<script src="/js/libs/backbone.paginator.js"></script>
-
 	<script src="/js/libs/uikit.min.js"></script>
 	<script src="/js/libs/pagination.js"></script>
-
-	<script src="/js/nav.js" type="text/jsx"></script>
-	<script src="/js/economy.js" type="text/jsx"></script>
-	<script src="/js/labaccess.js" type="text/jsx"></script>
-	<script src="/js/app.js" type="text/jsx"></script>
-
+	
 	<link rel="stylesheet" href="/css/uikit.min.css" />
 	<link rel="stylesheet" href="/css/uikit.almost-flat.css" />
-
 	<link rel="stylesheet" href="/css/default.css" />
 </head>
 <body>
+	<noscript>
+		Please turn on javascript in your browser.
+	</noscript>
+	<div class="loading-application">
+		<p>Loading...</p>
+	</div>
+
+	<script src="/js/app.js"></script>
 </body>
 </html>
