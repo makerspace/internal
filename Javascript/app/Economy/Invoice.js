078c0c55aacebebac5e7c5bce9ea52ee45dc8bb2
Christian Antila <tagged-github.com@chille.se>
2015-12-09 18:05:21 +0100

    Better layout on invoice

--- Javascript/app/Economy/Invoice.js
+++ Javascript/app/Economy/Invoice.js
@@ -90,8 +90,7 @@ var InvoiceList = React.createClass({
 				<th>#</th>
 				<th>Förfallodatum</th>
 				<th>Mottagare</th>
-				<th>Er referens</th>
-				<th>Vår referens</th>
+				<th>Referens</th>
 				<th className="uk-text-right">Belopp</th>
 				<th>Status</th>
 			</tr>
@@ -100,14 +99,18 @@ var InvoiceList = React.createClass({
 
 	renderRow: function(row, i)
 	{
+		if(row.status == "unpaid")
+		{
+			row.status = "Obetald";
+		}
+
 		return (
 			<tr key={i}>
 				<td><Link to={"/economy/invoice/" + row.invoice_number}>{row.invoice_number}</Link></td>
 				<td>{row.date_expiry}</td>
-				<td>{row.recipient}</td>
-				<td>{row.our_reference}</td>
+				<td>{row.title}</td>
 				<td>{row.your_reference}</td>
-				<td className="uk-text-right"><Currency value={row.priceGross} /></td>
+				<td className="uk-text-right"><Currency value={row._total} currency={row.currency} /></td>
 				<td>{row.status}</td>
 			</tr>
 		);
@@ -119,30 +122,114 @@ var Invoice = React.createClass({
 
 	render: function()
 	{
+		if(this.state.model.posts.length == 0)
+		{
+			var content = <tr><td colSpan="4"><em>Denna faktura saknar innehåll</em></td></tr>;
+		}
+		else
+		{
+			var currency = this.state.model.currency;
+			var content = this.state.model.posts.map(function (row, i)
+			{
+				// row.weight
+				// row.type
+
+				if(row.vat === null)
+				{
+					row.vat = "0%";
+				}
+
+				return (
+					<tr key={i}>
+						<td>{row.title}</td>
+						<td className="uk-text-right"><Currency value={row.price} currency={currency} /></td>
+						<td className="uk-text-right">{row.amount} {row.unit}</td>
+						<td className="uk-text-right"><Currency value={row._total} currency={currency} /></td>
+						<td className="uk-text-right">{row.vat}</td>
+					</tr>
+				);
+			})
+		}
+
 		return (
-			<div>
-				<dl>
-					<dt>#</dt>
-					<dd>{this.state.model.invoice_number}</dd>
+			<div className="invoice">
+				<a href={"/api/v2/economy/invoice/" + this.state.model.invoice_number + "/export"}>Exportera *.ODT</a>
 
-					<dt>Förfallodatum</dt>
-					<dd>{this.state.model.date_expiry}</dd>
+				<div className="uk-grid">
+					<div className="uk-width-1-3 box">
+						<div className="title">Mottagare</div>
+						<div className="data">{this.state.model.title}</div>
+					</div>
 
-					<dt>Mottagare</dt>
-					<dd>{this.state.model.recipient}</dd>
+					<div className="uk-width-1-3 box">
+						<div className="title">Belopp</div>
+						<div className="data"><Currency value={this.state.model._total} currency={currency} /></div>
+					</div>
 
-					<dt>Er referens</dt>
-					<dd>{this.state.model.your_reference}</dd>
+					<div className="uk-width-1-3 box">
+						<div className="title">Status</div>
+						<div className="data">{this.state.model.status}</div>
+					</div>
 
-					<dt>Vår referens</dt>
-					<dd>{this.state.model.our_reference}</dd>
+					<div className="uk-width-1-3 box">
+						<div className="title">Er referens</div>
+						<div className="data">{this.state.model.your_reference}</div>
+					</div>
 
-					<dt>Belopp</dt>
-					<dd>{this.state.model.amount}</dd>
+					<div className="uk-width-1-3 box">
+						<div className="title">Fakturadatum</div>
+						<div className="data">{this.state.model.date_invoice}</div>
+					</div>
 
-					<dt>Status</dt>
-					<dd>{this.state.model.status}</dd>
-				</dl>
+					<div className="uk-width-1-3 box">
+						<div className="title">Fakturanummer</div>
+						<div className="data">{this.state.model.invoice_number}</div>
+					</div>
+
+					<div className="uk-width-1-3 box">
+						<div className="title">Vår referens</div>
+						<div className="data">{this.state.model.our_reference}</div>
+					</div>
+
+					<div className="uk-width-1-3 box">
+						<div className="title">Förfallodatum</div>
+						<div className="data">{this.state.model.date_expiry}</div>
+					</div>
+
+					<div className="uk-width-1-3 box">
+						<div className="title">Betalningsvillkor</div>
+						<div className="data">{this.state.model.conditions} dagar</div>
+					</div>
+				</div>
+
+				<div className="box">
+					<div className="title">Kommentar</div>
+					<div className="data">
+						<pre>{this.state.model.description}</pre>
+					</div>
+				</div>
+
+				<div className="box">
+					<div className="title">Adress</div>
+					<div className="data">
+						<pre>{this.state.model.address}</pre>
+					</div>
+				</div>
+
+				<table className="uk-table uk-table-striped">
+					<thead>
+						<tr>
+							<th>Titel</th>
+							<th className="uk-text-right">Pris</th>
+							<th className="uk-text-right">Antal</th>
+							<th className="uk-text-right">Totalt</th>
+							<th className="uk-text-right">MOMS</th>
+						</tr>
+					</thead>
+					<tbody>
+						{content}
+					</tbody>
+				</table>
 			</div>
 		);
 	},
