602420293e30b6df92666844f0807076622b0e1b
Christian Antila <tagged-github.com@chille.se>
2015-12-15 18:36:27 +0100

    composer require cybermonde/odtphp

--- laravel/vendor/composer/autoload_real.php
+++ laravel/vendor/composer/autoload_real.php
@@ -41,15 +41,19 @@ class ComposerAutoloaderInit3b7a9c22cf6231450f7779faf8483b7e
         $loader->register(true);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $file) {
-            composerRequire3b7a9c22cf6231450f7779faf8483b7e($file);
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3b7a9c22cf6231450f7779faf8483b7e($fileIdentifier, $file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire3b7a9c22cf6231450f7779faf8483b7e($file)
+function composerRequire3b7a9c22cf6231450f7779faf8483b7e($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }
