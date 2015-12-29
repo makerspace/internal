e8543c673ea878d9e59cd9b6caf5a4937d340098
Christian Antila <tagged-github.com@chille.se>
2015-12-30 00:55:01 +0100

    Created new AccountingTransaction model

--- laravel/app/Http/Controllers/V2/EconomyTransactions.php
+++ laravel/app/Http/Controllers/V2/EconomyTransactions.php
@@ -5,10 +5,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-use DB;
+use App\Models\AccountingTransaction;
+use App\Traits\AccountingPeriod;
 
 class EconomyTransactions extends Controller
 {
+	use AccountingPeriod;
+
 	/**
 	 *
 	 */
@@ -35,51 +38,34 @@ class EconomyTransactions extends Controller
 	/**
 	 *
 	 */
-	function read(Request $request, $accountingperiod, $id)
+	function read(Request $request, $accountingperiod, $account_number)
 	{
-		// Get an list with id's of all accounting instructions which have a transaction to this account
-		$instructions = DB::table("entity")
-			->join("accounting_transaction", "accounting_transaction.entity_id", "=", "entity.entity_id")
+		// Check that the specified accounting period exists
+		$x = $this->_accountingPeriodOrFail($accountingperiod);
+		if(null !== $x)
+		{
+			return $x;
+		}
 
-			// Load the instruction
-			->join("accounting_instruction", "accounting_instruction.entity_id", "=", "accounting_transaction.accounting_instruction")
-			->join("entity AS ie", "ie.entity_id", "=", "accounting_instruction.entity_id")
+		// Load the transactions
+		$data = AccountingTransaction::list(
+			[
+				["account_number", "=", $account_number],
+				["accountingperiod", "=", $accountingperiod],
+			]
+		);
 
-			// Load the account
-			->join("accounting_account", "accounting_account.entity_id", "=", "accounting_transaction.accounting_account")
-			->join("entity AS ae", "ae.entity_id", "=", "accounting_account.entity_id")
-
-//			->where("accounting_transaction.accounting_account", $id)
-			->where("accounting_account.account_number", $id)
-			->select(
-				"accounting_account.account_number",
-				"ae.title AS account_name",
-				"accounting_instruction.instruction_number",
-				"ie.title AS instruction_title",
-				"entity.title AS transaction_title",
-//				"entity.description AS transaction_description",
-				"accounting_instruction.accounting_date",
-				"accounting_transaction.amount AS balance"
-			);
-//			->lists("accounting_instruction");
-
-		return $instructions->get();
-/*
-		// Load the accounting instructions
-		$data->instructions = DB::table("accounting_instruction")
-			->leftJoin("accounting_transaction", "accounting_instruction.id", "=", "accounting_transaction.accounting_instruction_id")
-			->groupBy("accounting_instruction.id")
-			->where("accounting_transaction.amount", ">", 0)
-			->select(
-				"accounting_instruction.id",
-				"accounting_instruction.accounting_date",
-				"accounting_instruction.verification_number",
-				"accounting_instruction.title",
-				"accounting_instruction.description",
-				DB::raw("SUM(accounting_transaction.amount) AS amount")
-			)
-			->get();
-			*/
+		// Generate an error if there is no such instruction
+		if(false === $data)
+		{
+			return Response()->json([
+				"message" => "No transactions found",
+			], 404);
+		}
+		else
+		{
+			return $data;
+		}
 	}
 
 	/**
--- laravel/app/Models/AccountingTransaction.php
+++ laravel/app/Models/AccountingTransaction.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Models;
+use DB;
 
 /*
 	protected $with = array('Account');
@@ -21,23 +22,44 @@ class AccountingTransaction extends Entity
 		"entity.entity_id"                              => "entity.entity_id",
 		"entity.created_at"                             => "DATE_FORMAT(entity.created_at, '%Y-%m-%dT%H:%i:%sZ') AS created_at",
 		"entity.updated_at"                             => "DATE_FORMAT(entity.updated_at, '%Y-%m-%dT%H:%i:%sZ') AS updated_at",
-		"entity.title"                                  => "entity.title",
-		"entity.description"                            => "entity.description",
+		"entity.title"                                  => "entity.title AS transaction_title",
+		"entity.description"                            => "entity.description AS transaction_description",
 		'accounting_transaction.accounting_instruction' => 'accounting_transaction.accounting_instruction',
 		'accounting_transaction.accounting_account'     => 'accounting_transaction.accounting_account',
 		'accounting_transaction.accounting_cost_center' => 'accounting_transaction.accounting_cost_center',
 		'accounting_transaction.amount'                 => 'accounting_transaction.amount',
 		'accounting_transaction.external_id'            => 'accounting_transaction.external_id',
 	];
-//	protected $sort = ["invoice_number", "desc"];
 
 	/**
 	 *
 	 */
 	public function _list($filters = [])
 	{
+//		DB::statement(DB::raw('SET @runtot = 0'));
+//		$query = $query
+
 		// Build base query
-		$query = $this->_buildLoadQuery();
+		$this->columns[] = "ie.title AS instruction_title";
+		$this->columns[] = "accounting_instruction.instruction_number";
+//		$this->columns[] = "12356 AS balance";
+//		$this->columns[] = "accounting_account.account_number";
+//		$this->columns[] = "ae.title AS account_name";
+		$this->columns[] = "accounting_instruction.accounting_date";
+
+		$this->columns[] = "accounting_instruction.external_id AS extid";
+
+		$query = $this->_buildLoadQuery()
+//			->selectRaw("(@runtot :=  amount + @runtot) AS balance")
+
+			// Load the instruction
+			->leftJoin("accounting_instruction", "accounting_instruction.entity_id", "=", "accounting_transaction.accounting_instruction")
+			->leftJoin("entity AS ie", "ie.entity_id", "=", "accounting_instruction.entity_id");
+
+			// Load the account
+//			->leftJoin("accounting_account", "accounting_account.entity_id", "=", "accounting_transaction.accounting_account")
+//			->leftJoin("entity AS ae", "ae.entity_id", "=", "accounting_account.entity_id");
+
 
 		// Go through filters
 		foreach($filters as $filter)
@@ -49,8 +71,57 @@ class AccountingTransaction extends Entity
 					->leftJoin("accounting_period", "accounting_period.entity_id", "=", "accounting_instruction.accounting_period")
 					->where("accounting_period.name", $filter[1], $filter[2]);
 			}
+			// Filter on accounting period
+			if("account_number" == $filter[0])
+			{
+				$query = $query
+					->leftJoin("accounting_account", "accounting_account.entity_id", "=", "accounting_transaction.accounting_account")
+					->where("accounting_account.account_number", $filter[1], $filter[2]);
+			}
 		}
 
-		return $query->get();
+		// Sort by accounting date
+		// TODO: This should not be used, sorting should be done via instruction number
+		$query = $query->orderBy("accounting_instruction.accounting_date", "asc");
+
+		$result = $query->get();
+		$data = [];
+		$balance = 0;
+		foreach($result as $row)
+		{
+/*
+			if(empty($row->instruction_number))
+			{
+				$row->instruction_number = "entity:".$row->entity_id;
+			}
+*/
+			$dir = "/var/www/html/vouchers/{$row->extid}";
+			if(file_exists($dir))
+			{
+				$row->files = "x";
+			}
+
+			$balance += $row->amount;
+			$row->balance = $balance;
+			$data[] = $row;
+		}
+		return $data;
 	}
 }
+
+/*
+		// Load the accounting instructions
+		$data->instructions = DB::table("accounting_instruction")
+			->leftJoin("accounting_transaction", "accounting_instruction.id", "=", "accounting_transaction.accounting_instruction_id")
+			->groupBy("accounting_instruction.id")
+			->where("accounting_transaction.amount", ">", 0)
+			->select(
+				"accounting_instruction.id",
+				"accounting_instruction.accounting_date",
+				"accounting_instruction.verification_number",
+				"accounting_instruction.title",
+				"accounting_instruction.description",
+				DB::raw("SUM(accounting_transaction.amount) AS amount")
+			)
+			->get();
+			*/
