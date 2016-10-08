d894e9e81935db5bd671d5d7a83df172a03a98ca
Christian Antila <tagged-github.com@chille.se>
2016-10-08 07:41:00 +0200

    Removed some of the old stuff from previous system built on CodeIgniter.

--- v1/application/config/makerspace.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Use storage of config in database
-$config['use_db_config'] = true;
--- v1/application/controllers/heartbeat.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Heartbeat extends CI_Controller {
-
-	public function cron() {
-		// Do nothing...
-	}
-}
