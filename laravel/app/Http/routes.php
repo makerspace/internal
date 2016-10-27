d0ff74c39aa11936b59835adb3fe5e606642f378
Christian Antila <tagged-github.com@chille.se>
2016-10-27 08:46:03 +0200

    Removed economy stuff and made into new micro services https://github.com/MakersOfSweden/MakerAdmin-Economy

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -13,9 +13,8 @@
 
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: authorization");
-//header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-//header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With");
 //header("Access-Control-Allow-Credentials: true");
 
 // API version 2
@@ -97,31 +96,6 @@ Route::group(["prefix" => "v1", "before" => "oauth"], function()
 	// Economy
 	Route::group(array("prefix" => "economy/{accountingperiod}"), function()
 	{
-		// Files / vouchers
-		Route::   get("file/{external_id}/{filename}", "V2\Economy@file"); // Get collection
-
-		// Transactions
-		Route::   get("transaction",      "V2\EconomyTransactions@list");   // Get collection
-		Route::  post("transaction",      "V2\EconomyTransactions@create"); // Model: Create
-		Route::   get("transaction/{id}", "V2\EconomyTransactions@read");   // Model: Read
-		Route::   put("transaction/{id}", "V2\EconomyTransactions@update"); // Model: Update
-		Route::delete("transaction/{id}", "V2\EconomyTransactions@delete"); // Model: Delete
-
-		// Instructions
-		Route::   get("instruction",      "V2\EconomyInstruction@list");     // Get collection
-		Route::  post("instruction",      "V2\EconomyInstruction@create");   // Model: Create
-		Route::   get("instruction/{id}", "V2\EconomyInstruction@read");     // Model: Read
-		Route::   put("instruction/{id}", "V2\EconomyInstruction@update");   // Model: Update
-		Route::delete("instruction/{id}", "V2\EconomyInstruction@delete");   // Model: Delete
-
-		// Accounts
-		Route::   get("masterledger",     "V2\EconomyAccount@masterledger"); // Get collection
-		Route::   get("account",          "V2\EconomyAccount@list");         // Get collection
-		Route::  post("account",          "V2\EconomyAccount@create");       // Model: Create
-		Route::   get("account/{id}",     "V2\EconomyAccount@read");         // Model: Read
-		Route::   put("account/{id}",     "V2\EconomyAccount@update");       // Model: Update
-		Route::delete("account/{id}",     "V2\EconomyAccount@delete");       // Model: Delete
-
 		// Invoices
 		Route::   get("invoice",             "V2\InvoiceController@list");   // Get collection
 		Route::  post("invoice",             "V2\InvoiceController@create"); // Model: Create
@@ -129,17 +103,6 @@ Route::group(["prefix" => "v1", "before" => "oauth"], function()
 		Route::   put("invoice/{id}",        "V2\InvoiceController@update"); // Model: Update
 		Route::delete("invoice/{id}",        "V2\InvoiceController@delete"); // Model: Delete
 		Route::   get("invoice/{id}/export", "V2\InvoiceController@export"); // Export *.odt
-
-		// Cost centers
-		Route::   get("costcenter",       "V2\EconomyCostcenter@list");   // Get collection
-		Route::  post("costcenter",       "V2\EconomyCostcenter@create"); // Model: Create
-		Route::   get("costcenter/{id}",  "V2\EconomyCostcenter@read");   // Model: Read
-		Route::   put("costcenter/{id}",  "V2\EconomyCostcenter@update"); // Model: Update
-		Route::delete("costcenter/{id}",  "V2\EconomyCostcenter@delete"); // Model: Delete
-
-		// Reports
-		Route::   get("valuationsheet",   "V2\EconomyReport@valuationSheet"); // Get collection
-		Route::   get("resultreport",     "V2\EconomyReport@resultReport");   // Get collection
 	});
 
 	// Relations
@@ -151,7 +114,7 @@ Route::group(["prefix" => "v1", "before" => "oauth"], function()
 	Route::delete("relation/{id}", "V2\Relation@delete"); // Delete relation
 });
 
-// Everything else should load the single page app
+// Everything else should give an error
 Route::get("/{wildcard}", function ()
 {
 	return Response()->json([
